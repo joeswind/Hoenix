1e1c2bc5b661216beb571f6ae96c407b3d8c5787
joeswind <joeswind0115@gmail.com>
2017-03-22 18:13:54 +0800

    ContactMe.JS modify
    修改為Google表單紀錄

--- js/contact_me.js
+++ js/contact_me.js
@@ -20,14 +20,15 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
-                type: "POST",
-                data: {
-                    name: name,
-                    phone: phone,
-                    email: email,
-                    message: message
-                },
+                url: "https://docs.google.com/forms/d/e/1FAIpQLSdSBa-zEJ9sAe-j56_zBzh5DSR_AjmuR0ifd5PS9OJZ5IQ1ig/formResponse?" + "entry.546953780=" + name
+                  + "&entry.1178561295=" + phone +  "&entry.12321975=" + email + "&entry.40212249=" + message + "&submit=Submit",
+                type: "GET",
+                /*data: {
+                    "entry.546953780": name,
+                    "entry.1178561295": phone,
+                    "entry.12321975": email,
+                    "entry.40212249": message
+                },*/
                 cache: false,
                 success: function() {
                     // Success message
@@ -35,22 +36,32 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>您的訊息已經成功送出，為確保訊息不要丟失，請盡速與我們電話聯絡，謝謝. </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
                     //clear all fields
                     $('#contactForm').trigger("reset");
                 },
-                error: function() {
-                    // Fail message
+                error: function(xhr, textStatus) {
+                    $('#success').html("<div class='alert alert-success'>");
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-success')
+                        .append("<strong>您的訊息已經成功送出，為確保訊息不要丟失，請盡速與我們電話聯絡，謝謝</strong>");
+                    $('#success > .alert-success')
+                        .append('</div>');
+
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                    /*// Fail message
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+                    $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", 糟糕現在MAIL SERVER好像出了點問題，請您改用電話聯絡，謝謝"));
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
-                    $('#contactForm').trigger("reset");
+                    $('#contactForm').trigger("reset");*/
                 },
             });
         },
